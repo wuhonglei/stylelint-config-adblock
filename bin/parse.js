aa6e6e8812c29757b14d1984e9ffa12ea25b001f
Honglei Wu <honglei.wu@shopee.com>
2022-05-12 23:09:22 +0800

    完成 ab block stylint 规则整理

---
+++ bin/parse.js
@@ -0,0 +1,48 @@
+// 从 data/easylistchina+easylist.txt 文件中提取 class selector，并存放到 src/classSelector.js
+
+const fs = require('fs');
+const path = require('path');
+
+const filepath = {
+  // easylist 数据来源：https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt
+  easylist: path.resolve(
+    __dirname,
+    '../data/easylistchina+easylist_20220512.txt'
+  ),
+  output: path.join(__dirname, '../lib/classSelector.js')
+};
+
+function getFileContentList(src) {
+  const content = fs.readFileSync(src, {
+    encoding: 'utf8',
+    flag: 'r'
+  });
+  return content || '';
+}
+
+function writeContentToFile(rowList) {
+  const content = `module.exports = ${JSON.stringify(rowList, null, 2)};\n`;
+  fs.writeFileSync(filepath.output, content, { encoding: 'utf8' });
+}
+
+function isClassSelector(str) {
+  const pattern = /^(##)(\.)([a-z])/;
+  return pattern.test(str);
+}
+
+function extractClassSelector(str) {
+  return str.replace(/^##/, '');
+}
+
+// --------------------------- //
+function main() {
+  const content = getFileContentList(filepath.easylist);
+  const rowList = content.split('\n');
+  const selectorList = rowList
+    .filter(isClassSelector)
+    .map(extractClassSelector);
+
+  writeContentToFile(selectorList);
+}
+
+main();
